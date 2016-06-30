abd228442433be0bf77ae6e2a560857c53a2952c
Eric Sun <yesterdaysun@gmail.com>
2016-06-30 22:12:13 +0800

    define all pieces

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,21 @@
-import java.util.Scanner;
-
 /**
  * Created by Eric on 16/6/30.
  */
 public class Application {
     public static void main(String[] args) {
         Pyramid pyramid = new Pyramid();
-        pyramid.put(Piece.A3,0,0);
-        pyramid.put(Piece.A5,0,5);
+        pyramid.put(Piece.A4, 6, 0);
+        pyramid.put(Piece.B1, 2, 0);
+        pyramid.put(Piece.C6, 0, 5);
+        pyramid.put(Piece.D4, 2, 4);
+        pyramid.put(Piece.E8, 5, 0);
+        pyramid.put(Piece.F1, 8, 0);
+        pyramid.put(Piece.G2, 0, 2);
+        pyramid.put(Piece.H2, 3, 3);
+        pyramid.put(Piece.I4, 1, 3);
+        pyramid.put(Piece.J2, 0, 6);
+        pyramid.put(Piece.K, 0, 0);
+        pyramid.put(Piece.L, 3, 1);
         pyramid.print();
     }
 }
--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,7 +1,5 @@
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -84,16 +82,305 @@ public class Piece {
     public static Piece A7;
     public static Piece A8;
 
+    public static Piece B1;
+    public static Piece B2;
+    public static Piece B3;
+    public static Piece B4;
+    public static Piece B5;
+    public static Piece B6;
+    public static Piece B7;
+    public static Piece B8;
+
+    public static Piece C1;
+    public static Piece C2;
+    public static Piece C3;
+    public static Piece C4;
+    public static Piece C5;
+    public static Piece C6;
+    public static Piece C7;
+    public static Piece C8;
+
+    public static Piece D1;
+    public static Piece D2;
+    public static Piece D3;
+    public static Piece D4;
+    public static Piece D5;
+    public static Piece D6;
+    public static Piece D7;
+    public static Piece D8;
+
+    public static Piece E1;
+    public static Piece E2;
+    public static Piece E3;
+    public static Piece E4;
+    public static Piece E5;
+    public static Piece E6;
+    public static Piece E7;
+    public static Piece E8;
+
+    public static Piece F1;
+    public static Piece F2;
+    public static Piece F3;
+    public static Piece F4;
+
+    public static Piece G1;
+    public static Piece G2;
+    public static Piece G3;
+    public static Piece G4;
+
+    public static Piece H1;
+    public static Piece H2;
+    public static Piece H3;
+    public static Piece H4;
+
+    public static Piece I1;
+    public static Piece I2;
+    public static Piece I3;
+    public static Piece I4;
+
+    public static Piece J1;
+    public static Piece J2;
+
+    public static Piece K;
+
+    public static Piece L;
+
     static {
+        //     A
+        // A A A
         A1 = new Piece(TYPE_A, new Point(2, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1));
+        // A
+        // A
+        // A A
         A2 = A1.rotate(90);
+        // A A A
+        // A
         A3 = A2.rotate(90);
+        // A A
+        //   A
+        //   A
         A4 = A3.rotate(90);
+        // A A A
+        //     A
         A5 = A1.flip();
+        //   A
+        //   A
+        // A A
         A6 = A5.rotate(90);
+        // A
+        // A A A
         A7 = A6.rotate(90);
+        // A A
+        // A
+        // A
         A8 = A7.rotate(90);
 
+        // B B B
+        // B B
+        B1 = new Piece(TYPE_B, new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1));
+        // B B
+        // B B
+        //   B
+        B2 = B1.rotate(90);
+        //   B B
+        // B B B
+        B3 = B2.rotate(90);
+        // B
+        // B B
+        // B B
+        B4 = B3.rotate(90);
+        // B B
+        // B B B
+        B5 = B1.flip();
+        // B B
+        // B B
+        // B
+        B6 = B5.rotate(90);
+        // B B B
+        //   B B
+        B7 = B6.rotate(90);
+        //   B
+        // B B
+        // B B
+        B8 = B7.rotate(90);
+
+        // C
+        // C C C C
+        C1 = new Piece(TYPE_C, new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1));
+        // C C
+        // C
+        // C
+        // C
+        C2 = C1.rotate(90);
+        // C C C C
+        //       C
+        C3 = C2.rotate(90);
+        //   C
+        //   C
+        //   C
+        // C C
+        C4 = C3.rotate(90);
+        // C C C C
+        // C
+        C5 = C1.flip();
+        // C C
+        //   C
+        //   C
+        //   C
+        C6 = C5.rotate(90);
+        //       C
+        // C C C C
+        C7 = C6.rotate(90);
+        // C
+        // C
+        // C
+        // C C
+        C8 = C7.rotate(90);
+
+        // D D D D
+        //   D
+        D1 = new Piece(TYPE_D, new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0), new Point(1, 1));
+        //   D
+        // D D
+        //   D
+        //   D
+        D2 = D1.rotate(90);
+        //     D
+        // D D D D
+        D3 = D2.rotate(90);
+        // D
+        // D
+        // D D
+        // D
+        D4 = D3.rotate(90);
+        //   D
+        // D D D D
+        D5 = D1.flip();
+        // D
+        // D D
+        // D
+        // D
+        D6 = D2.rotate(90);
+        // D D D D
+        //     D
+        D7 = D3.rotate(90);
+        //   D
+        //   D
+        // D D
+        //   D
+        D8 = D4.rotate(90);
+
+        // E E E
+        //     E E
+        E1 = new Piece(TYPE_E, new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(2, 1), new Point(3, 1));
+        //   E
+        //   E
+        // E E
+        // E
+        E2 = E1.rotate(90);
+        // E E
+        //   E E E
+        E3 = E2.rotate(90);
+        //   E
+        // E E
+        // E
+        // E
+        E4 = E3.rotate(90);
+        //     E E
+        // E E E
+        E5 = E1.flip();
+        // E
+        // E
+        // E E
+        //   E
+        E6 = E5.rotate(90);
+        //   E E E
+        // E E
+        E7 = E6.rotate(90);
+        // E
+        // E E
+        //   E
+        //   E
+        E8 = E7.rotate(90);
+
+        // F F
+        // F
+        F1 = new Piece(TYPE_F, new Point(0, 0), new Point(0, 1), new Point(1, 0));
+        // F F
+        //   F
+        F2 = F1.rotate(90);
+        //   F
+        // F F
+        F3 = F2.rotate(90);
+        // F
+        // F F
+        F4 = F3.rotate(90);
+
+        // G
+        // G
+        // G G G
+        G1 = new Piece(TYPE_G, new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(1, 2), new Point(2, 2));
+        // G G G
+        // G
+        // G
+        G2 = G1.rotate(90);
+        // G G G
+        //     G
+        //     G
+        G3 = G2.rotate(90);
+        //     G
+        //     G
+        // G G G
+        G4 = G3.rotate(90);
+
+        // H H
+        //   H H
+        //     H
+        H1 = new Piece(TYPE_H, new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1), new Point(2, 2));
+        //     H
+        //   H H
+        // H H
+        H2 = H1.rotate(90);
+        // H
+        // H H
+        //   H H
+        H3 = H2.rotate(90);
+        //   H H
+        // H H
+        // H
+        H4 = H3.rotate(90);
+
+        // I I
+        //   I
+        // I I
+        I1 = new Piece(TYPE_I, new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 2));
+        // I   I
+        // I I I
+        I2 = I1.rotate(90);
+        // I I
+        // I
+        // I I
+        I3 = I2.rotate(90);
+        // I I I
+        // I   I
+        I4 = I3.rotate(90);
+
+        // J J J J
+        J1 = new Piece(TYPE_J, new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0));
+        // J
+        // J
+        // J
+        // J
+        J2 = J1.rotate(90);
+
+        // K K
+        // K K
+        K = new Piece(TYPE_K, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(1, 1));
+
+        //   L
+        // L L L
+        //   L
+        L = new Piece(TYPE_L, new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(1, 2));
     }
 
     private int pieceNumber;
